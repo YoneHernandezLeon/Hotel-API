d7d101a31f0a5ac4605871c4d29cc7fa74e9402d
Yone Hernández <yone.hdez.leon@gmail.com>
2025-12-30 14:24:49 +0000

    test: :test_tube: modify test to fail
    mae test fail to check for id and hotel name

--- src/test/java/com/yhernandez/hotel_api/controller/HotelApiControllerTest.java
+++ src/test/java/com/yhernandez/hotel_api/controller/HotelApiControllerTest.java
@@ -3,14 +3,21 @@ package com.yhernandez.hotel_api.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.yhernandez.hotel_api.services.HotelApiService;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Map;
 
 @WebMvcTest(HotelApiController.class)
 public class HotelApiControllerTest {
@@ -36,7 +43,24 @@ public class HotelApiControllerTest {
                 }
                 """;
 
-        mockMvc.perform(post("/hotels").content(body).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated());
+        String responseBody = """
+                {
+                    "hotel_name": "Hotel Gran Canaria",
+                    "id": "1"
+                }
+                """;
+
+        when(hotelApiService.createHotel(any()))
+                .thenReturn(ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body(Map.of(
+                                "id", "1",
+                                "hotel_name", "Hotel Gran Canaria")));
+
+        mockMvc.perform(post("/hotels")
+                .content(body)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(content().json(responseBody));
     }
 }
